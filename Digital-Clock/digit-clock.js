e0ea771f5dfd622bfc54ad58c13f6f8700b012d7
DEVISS <104900700+ICDEIS@users.noreply.github.com>
2022-06-10 21:47:46 +0500

    date added

--- Digital-Clock/digit-clock.js
+++ Digital-Clock/digit-clock.js
@@ -6,11 +6,22 @@ function realTime() {
    const realMinutes = time.getMinutes()
    const realSeconds = time.getSeconds()
 
+   const realWeek = time.getDay()
+   const realMonth = time.getMonth()
+   const realDay = time.getDate()
+   const realYear = time.getFullYear()
+
    const hour = document.getElementsByClassName('hours')[0]
    const minute = document.getElementsByClassName('minutes')[0]
    const second = document.getElementsByClassName('seconds')[0]
    const session = document.getElementsByClassName('session')[0]
 
+   const week = document.getElementsByClassName('week')[0]
+   const month = document.getElementsByClassName('month')[0]
+   const day = document.getElementsByClassName('day')[0]
+   const year = document.getElementsByClassName('year')[0]
+
+   
    if(realHours >= 12) {
       session.innerHTML = 'PM'
    } if(realHours < 12) {
@@ -24,9 +35,58 @@ function realTime() {
    } else {
       second.innerHTML = realSeconds
    }
-   
    minute.innerHTML = realMinutes
+   
+   weekDay = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+   if(realWeek == 0)
+   week.innerHTML = weekDay[0]
+   if(realWeek == 1)
+   week.innerHTML = weekDay[1]
+   if(realWeek == 2)
+   week.innerHTML = weekDay[2]
+   if(realWeek == 3)
+   week.innerHTML = weekDay[3]
+   if(realWeek == 4)
+   week.innerHTML = weekDay[4]
+   if(realWeek == 5)
+   week.innerHTML = weekDay[5]
+   if(realWeek == 6)
+   week.innerHTML = weekDay[6]
+   
+   if(realDay < 10) {
+      day.innerHTML = `0${realDay}`
+   } else {
+      day.innerHTML = realDay
+   }
+   
+   months = ['January','February','March','April','May','June','July','August','September','Octobe','November','December']
+   if(realMonth == 0)
+      month.innerHTML = months[0]
+   if(realMonth == 1)
+      month.innerHTML = months[1]
+   if(realMonth == 2)
+      month.innerHTML = months[2]
+   if(realMonth == 3)
+      month.innerHTML = months[3]
+   if(realMonth == 4)
+      month.innerHTML = months[4]
+   if(realMonth == 5)
+      month.innerHTML = months[5]
+   if(realMonth == 6)
+      month.innerHTML = months[6]
+   if(realMonth == 7)
+      month.innerHTML = months[7]
+   if(realMonth == 8)
+      month.innerHTML = months[8]
+   if(realMonth == 9)
+      month.innerHTML = months[9]
+   if(realMonth == 10)
+      month.innerHTML = months[10]
+   if(realMonth == 11)
+      month.innerHTML = months[11]
+
+   year.innerHTML = realYear
+
 }
 realTime()
 setInterval(realTime, 1000)
-
