a1fde4545bf43c3ae2a7ee12aeb544e04fa090e6
DEVISS <104900700+ICDEIS@users.noreply.github.com>
2022-06-10 19:56:15 +0500

    digital clock finishid

---
+++ Digital-Clock/digit-clock.js
@@ -0,0 +1,32 @@
+
+
+function realTime() {
+   const time = new Date()
+   const realHours = time.getHours()
+   const realMinutes = time.getMinutes()
+   const realSeconds = time.getSeconds()
+
+   const hour = document.getElementsByClassName('hours')[0]
+   const minute = document.getElementsByClassName('minutes')[0]
+   const second = document.getElementsByClassName('seconds')[0]
+   const session = document.getElementsByClassName('session')[0]
+
+   if(realHours >= 12) {
+      session.innerHTML = 'PM'
+   } if(realHours < 12) {
+      session.innerHTML = 'AM'
+   } if(realHours < 12) {
+      hour.innerHTML = realHours
+   } if(realHours > 12) {
+      hour.innerHTML = (realHours - 12)
+   } if(realSeconds < 10) {
+      second.innerHTML = `0${realSeconds}`
+   } else {
+      second.innerHTML = realSeconds
+   }
+   
+   minute.innerHTML = realMinutes
+}
+
+setInterval(realTime, 1)
+
